d59b0d5ba4e58d7250b4a45d79fb4dcad50f4539
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2025-06-22 15:36:09 +0530

    initial commit

---
+++ machines/seedbox/configuration.nix
@@ -0,0 +1,29 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+}@args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.systemd-boot.enable = true;
+  services.openssh.enable = true;
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+    pkgs.neovim
+    pkgs.git
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    # change this to your ssh key
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9cZDQjzlmMTw2PStQMWlk+XbLfP4KMKeKM3C9izsyf kvineet@kvineet-pc"
+  ] ++ (args.extraPublicKeys or [ ]); # this is used for unit-testing this module and can be removed if not needed
+
+  system.stateVersion = "24.05";
+}
